865645171947b82472c1998e4f169a84ffb9c828
xervii <ahtaullahakmali@gmail.com>
2026-02-01 18:12:41 +0700

    Exploring math.h header file

---
+++ The basics/05_mathLibrary.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()  {
+    double x = 3.14;
+    double y = 9.0;
+    double z = 0.0;
+
+    z = sqrt(y);
+
+    printf("%.2lf", z);
+    return 0;
+}
+
+/*
+Lazy to add all, but all of math.h contents
+But all of math.h contents could be 
+found in Geeksforgeeks site
+*/
