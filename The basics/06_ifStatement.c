fbfb438fbe8011a502940979d3d391df659b4ddf
xervii <ahtaullahakmali@gmail.com>
2026-02-02 12:23:53 +0700

    Learning nested if statements

---
+++ The basics/06_ifStatement.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main()  {
+    int age = 67;
+    bool isStudent = true;
+
+    if(age >= 65) {
+        if(isStudent) {
+            printf("30 percent discount!");
+        }
+        else{
+            printf("20 percent discount!");
+        }
+    }
+    else if (age >= 18) {
+        if(isStudent) {
+            printf("20 percent discount!");
+        }
+        else {
+            printf("10 percent discount!");
+        }
+    }
+    else {
+        printf("No discount!");
+    }
+    return 0;
+}
