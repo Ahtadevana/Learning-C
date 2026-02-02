827f76a6736041e18910ed8f88ec623998dac5de
xervii <ahtaullahakmali@gmail.com>
2026-02-02 19:53:23 +0700

    Making a weight converter program

---
+++ The basics/projectF_temperatureConverter.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main()  {
+    char choice = '\0';
+    float temp = 0.0f;
+    double result = 0.0;
+
+    printf("---Welcome to the temperature converter program!---\n");
+    printf("Insert temperature: ");
+    scanf("%f", &temp);
+    
+    printf("---options---\n");
+    printf("'C' to convert Celsius to Fahrenheit\n");
+    printf("'F' to convert Fahrenheit to Celsius\n");
+    printf("Your choice: ");
+    scanf(" %c", &choice);
+    choice = toupper(choice);
+
+    if(choice == 'C') {
+        result = (temp * 9.0 / 5.0) + 32;
+    } else if(choice == 'F') {
+        result = (temp - 32) * 5.0 / 9.0;
+    }
+    printf("The converted temperature is: %.2lf", result);
+
+    return 0;
+}
