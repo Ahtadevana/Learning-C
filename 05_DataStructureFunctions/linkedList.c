073a25937f0f40b72f9ab4d9abbc0e68a3b87f34
xervii <ahtaullahakmali@gmail.com>
2026-03-13 05:21:31 +0700

    Im still confused...

--- 05_DataStructureFunctions/linkedList.c
+++ 05_DataStructureFunctions/linkedList.c
@@ -9,18 +9,40 @@ typedef struct node {
 
 Node* createNode(int data);
 void insertAtHead(Node **head, int data);
-void insertAtValue(Node **head, int data);
+void insertAfterValue(Node *head, int key, int data);
 void insertAtTail(Node **head, int data);
-void searchNode(Node *head, int data);
-void printNode(Node *head);
+
+void zeroList(Node **head);
+void deleteNode(Node **head, int key);
+
+void listLen(Node *head);
+void searchNode(Node *head, int key);
+void printList(Node *head);
 
 int main() {
-    Node *head = createNode(10);
-    insertAtHead(&head, 20);
-    insertAtHead(&head, 40);
-    printNode(head);
+    Node *head = createNode(20);
+    printList(head);
 
-    searchNode(head, 10);
+    insertAtHead(&head, 10);
+    insertAtHead(&head, 0);
+    printList(head);
+    listLen(head);
+
+    insertAtTail(&head, 30);
+    insertAtTail(&head, 40);
+    printList(head);
+
+    insertAfterValue(head, 20, 25);
+    insertAfterValue(head, 30, 35);
+    printList(head);
+
+    searchNode(head, 20);
+    insertAtTail(&head, 50);
+    deleteNode(&head, 30);
+    printList(head);
+
+    listLen(head);
+
     return 0;
 }
 
@@ -41,6 +63,22 @@ void insertAtHead(Node **head, int data){
     *head = newNode;
 }
 
+void insertAfterValue(Node *head, int key, int data){
+    Node *newNode = createNode(data);
+    Node *temp = head;
+
+    while(temp != NULL){
+        if(temp->data == key){
+            newNode->next = temp->next;
+            temp->next = newNode;
+            printf("Data found! Allocated after %d\n", key);
+            return;
+        }
+        temp = temp->next;
+    }
+    printf("Data %d NOT found! Allocation failed!\n", key);
+}
+
 void insertAtTail(Node **head, int data){
     Node *newNode = createNode(data);
     if(*head == NULL){
@@ -57,24 +95,39 @@ void insertAtTail(Node **head, int data){
     }
 }
 
-void searchNode(Node *head, int data){
+void zeroList(Node **head){
+    for(Node *current = *head; current != NULL; current = current->next){
+        current->data = 0;
+    }
+    printf("All datas are ZERO-ed!\n");
+}
+
+void listLen(Node *head){
+    int count;
+    for(Node *current = head; current != NULL; current = current->next){
+        count++;
+    }
+    printf("List length: %d\n", count);
+}
+
+void searchNode(Node *head, int key){
     bool found = false;
     int count = 0;
     for(Node *current = head; current != NULL; current = current->next){
         count++;
-        if(current->data == data){
+        if(current->data == key){
             found = true;
             break;
         }
     }
     if(found){
-        printf("Found at #%d Node!", count);
+        printf("Found at #%d Node from head!\n", count);
     } else {
-        printf("Not Found!");
+        printf("%d Not Found!\n", key);
     }
 }
 
-void printNode(Node *head){
+void printList(Node *head){
     Node *temp = head;
 
     while(temp != NULL){
